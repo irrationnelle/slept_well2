489ca77a8565db35a65063ca4b9cdbec71d2fd54
irrationnelle <drakkarverenis@gmail.com>
2017-02-16 15:45:59 +0900

    PREVIOUS:  new brunch

--- BandMaker/models.py
+++ BandMaker/models.py
@@ -1,7 +1,38 @@
 from django.db import models
 from Applicants.models import MyUser
+from Session.models import Session, Instrument
 
 # Create your models here.
 class Band(models.Model):
-    master = models.ForeignKey(MyUser, verbose_name='band master', related_name='band')
-    # members = models.ForeignKey(MyUser, verbose_name="session members", related_name='members')
+    master = models.ForeignKey(
+        MyUser,
+        verbose_name='band master',
+        related_name='%(app_label)s_%(class)s_master'
+    )
+
+    sessions = models.ManyToManyField(
+        Session,
+        verbose_name='band session list',
+        related_name='%(app_label)s_%(class)s_sessions'
+    )
+
+    name = models.CharField(max_length=252, verbose_name='band name')
+
+    musicPieces = models.ManyToManyField(
+        Instrument,
+        verbose_name='band music list',
+        related_name='%(app_label)s_%(class)s_musics'
+    )
+
+class MusicPiece(models.Model):
+    name = models.CharField(max_length=252, verbose_name='music name')
+
+    datetime = models.DateTimeField(verbose_name='perfome Date and time')
+
+    instruments = models.ManyToManyField(
+        Instrument,
+        verbose_name='required instruments',
+        related_name='%(app_label)s_%(class)s_related'
+    )
+
+    youtubeURL = models.URLField()
--- Session/models.py
+++ Session/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from Applicants.models import MyUser
 
 # Create your models here.
+class Session(models.Model):
+    players = models.ForeignKey(
+        MyUser,
+        verbose_name='own user info',
+        related_name='%(app_label)s_%(class)s_related'
+    )
+
+    instruments = models.ManyToManyField(Instrument)
+
+class Instrument(models.Model):
+    name = models.CharField(max_length=50, verbose_name='instrument name')
+
